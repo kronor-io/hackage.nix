8ba452ead01afc3e0d5635be9da576a019baa562
pranaysashank <pranaysashank@users.noreply.github.com>
2025-02-23 00:16:35 +0000

    Automatic Update

---
+++ nix/derive-is-data-con.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "9be7e18975bf5eb6c4967206e11a531182a39720209574546295b5c9a9410c71";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/derive-is-data-con-0.1.0.0-r0-eba86c4d21da666bfec179350851aaccfe5ec1216f6d5b4435657ed870a180f9.nix;
+        revNum = 0;
+        revTimestamp = "2025-02-22T12:51:33Z";
+        sha256 = "eba86c4d21da666bfec179350851aaccfe5ec1216f6d5b4435657ed870a180f9";
+        };
+      default = "r0";
+      };
+    };
+  }
