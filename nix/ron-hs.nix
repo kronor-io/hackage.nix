8b03c90c1f6e9925ab15a5189bb04858bf3beabc
pranaysashank <pranaysashank@users.noreply.github.com>
2025-03-08 00:15:05 +0000

    Automatic Update

---
+++ nix/ron-hs.nix
@@ -0,0 +1,14 @@
+{
+  "0.4.0" = {
+    sha256 = "92f9327f068773238e3719760efe556b1cdd659a797765f5b369924bc25540f9";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/ron-hs-0.4.0-r0-cb8e4f951549854d3c3531a2c3ba40e5fdbed2cc993852aaccf3f258ea64ee5d.nix;
+        revNum = 0;
+        revTimestamp = "2025-03-07T21:59:04Z";
+        sha256 = "cb8e4f951549854d3c3531a2c3ba40e5fdbed2cc993852aaccf3f258ea64ee5d";
+        };
+      default = "r0";
+      };
+    };
+  }
