928265cfef7e1a4cfa0ddc96f5c181394885b396
pranaysashank <pranaysashank@users.noreply.github.com>
2025-08-29 00:26:08 +0000

    Automatic Update

---
+++ nix/dawgdic.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0" = {
+    sha256 = "4efb8fe1c1e6c0f1edd8842c670f3c61df12da083b466b89470d04650f575f4a";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/dawgdic-0.1.0-r0-a2ecad683aec8f222f4fa8ce7902832bbb81c60606ce0511e4dbf1f905d012a2.nix;
+        revNum = 0;
+        revTimestamp = "2025-08-28T19:22:45Z";
+        sha256 = "a2ecad683aec8f222f4fa8ce7902832bbb81c60606ce0511e4dbf1f905d012a2";
+      };
+      default = "r0";
+    };
+  };
+}
