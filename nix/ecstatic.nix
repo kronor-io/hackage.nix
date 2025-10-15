b2dfe5944b1fc58482ed80e441d2d1f619d60c79
pranaysashank <pranaysashank@users.noreply.github.com>
2025-10-15 00:41:18 +0000

    Automatic Update

---
+++ nix/ecstatic.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "bcfeec9df2dbb03d48221a256d5b8cb667934da58435a669e069c04f8b85f8d0";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/ecstatic-0.1.0.0-r0-15224851e787ca808b4e100a229324c7a9400860d75512efdf30173d3ea72454.nix;
+        revNum = 0;
+        revTimestamp = "2025-10-14T21:59:11Z";
+        sha256 = "15224851e787ca808b4e100a229324c7a9400860d75512efdf30173d3ea72454";
+      };
+      default = "r0";
+    };
+  };
+}
