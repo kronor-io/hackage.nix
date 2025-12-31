05b45fd9de004a05f9daee1b0fcc86d8454eca95
pranaysashank <pranaysashank@users.noreply.github.com>
2025-12-31 00:44:43 +0000

    Automatic Update

---
+++ nix/kb-text-shape.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "5a9dab43c3e363c699c091bc24f2274e416622d96eb6f2b86a60bcee96bdf8cd";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/kb-text-shape-0.1.0.0-r0-fdd28ca2e7ca7876601999544813dfce8629eb314596b8b70281bad064422c66.nix;
+        revNum = 0;
+        revTimestamp = "2025-12-30T17:20:44Z";
+        sha256 = "fdd28ca2e7ca7876601999544813dfce8629eb314596b8b70281bad064422c66";
+      };
+      default = "r0";
+    };
+  };
+}
---
+++ nix/msdf-atlas.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "ec77b87008224ea6819752122479b3307ab607ffee66e578f8391ff0738482cd";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/msdf-atlas-0.1.0.0-r0-d6dd2d5623bdafcb282b7a0d30f67085fafe438f6d30683f0dc1cb8efdb188b5.nix;
+        revNum = 0;
+        revTimestamp = "2025-12-30T17:22:38Z";
+        sha256 = "d6dd2d5623bdafcb282b7a0d30f67085fafe438f6d30683f0dc1cb8efdb188b5";
+      };
+      default = "r0";
+    };
+  };
+}
