fbc2399558fac1a3b986aa6ae1ef957fb1cdc65e
pranaysashank <pranaysashank@users.noreply.github.com>
2026-02-03 00:49:21 +0000

    Automatic Update

--- nix/dependent-enummap.nix
+++ nix/dependent-enummap.nix
@@ -8,7 +8,13 @@
         revTimestamp = "2025-05-13T12:17:04Z";
         sha256 = "d338d5cadc4241de0f1b4e8dd1df6065b6535c14770ddfef851aba1aa5672eaa";
       };
-      default = "r0";
+      r1 = {
+        nix = import ../hackage/dependent-enummap-0.1.0.0-r1-af7384420f0aa5b75a7fef729a2df7c45f5245956c2d3f3a2e549489e9ee0da9.nix;
+        revNum = 1;
+        revTimestamp = "2026-02-02T21:15:58Z";
+        sha256 = "af7384420f0aa5b75a7fef729a2df7c45f5245956c2d3f3a2e549489e9ee0da9";
+      };
+      default = "r1";
     };
   };
 }
---
+++ nix/pandoc-typescript.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "38bf5cd0ca515cbeb9b4f9277b7199bd64422315b8531c7d042982cdd50c655e";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/pandoc-typescript-0.1.0.0-r0-653a900dfa27aca0fc65b27a4841cea19acdcfe5cc1daddbd27e4348a90127dd.nix;
+        revNum = 0;
+        revTimestamp = "2026-02-02T01:15:31Z";
+        sha256 = "653a900dfa27aca0fc65b27a4841cea19acdcfe5cc1daddbd27e4348a90127dd";
+      };
+      default = "r0";
+    };
+  };
+}
