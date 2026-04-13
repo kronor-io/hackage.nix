070e4046b0cfc13e8129b2573ad7687841ad663f
pranaysashank <pranaysashank@users.noreply.github.com>
2026-04-13 00:56:13 +0000

    Automatic Update

---
+++ nix/hs-wayland-scanner.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0" = {
+    sha256 = "67ea6526b2c99551fe0060749baf712dd126d665b96513974e89a33e16bc4574";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/hs-wayland-scanner-0.1.0-r0-334c1921b5b190ef9c6d7dd52207fe96ac241dd149b04c66ce433462a3feca85.nix;
+        revNum = 0;
+        revTimestamp = "2026-04-12T15:46:36Z";
+        sha256 = "334c1921b5b190ef9c6d7dd52207fe96ac241dd149b04c66ce433462a3feca85";
+      };
+      default = "r0";
+    };
+  };
+}
--- nix/safe-coloured-text-gen.nix
+++ nix/safe-coloured-text-gen.nix
@@ -47,4 +47,16 @@
       default = "r0";
     };
   };
+  "0.0.0.4" = {
+    sha256 = "9b882ea03c31d1a7683cf235b95d15527c2212d8cc94b8a74a2fd93746aecc68";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/safe-coloured-text-gen-0.0.0.4-r0-04182359ec1cc44871aeb5fac7b5f5077b546bdb1f3d7ea977c8cf1f86ecdc4a.nix;
+        revNum = 0;
+        revTimestamp = "2026-04-12T11:15:32Z";
+        sha256 = "04182359ec1cc44871aeb5fac7b5f5077b546bdb1f3d7ea977c8cf1f86ecdc4a";
+      };
+      default = "r0";
+    };
+  };
 }
