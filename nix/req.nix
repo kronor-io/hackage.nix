f3e78f30a8c4bda23a1830c7d096dbc767068181
pranaysashank <pranaysashank@users.noreply.github.com>
2026-05-02 01:00:48 +0000

    Automatic Update

--- nix/req.nix
+++ nix/req.nix
@@ -506,7 +506,13 @@
         revTimestamp = "2026-03-18T08:38:52Z";
         sha256 = "40c7f8a7faee32385d3a97f6180ce9907cf2159fa2aaa193bdd3ee3ec74d6049";
       };
-      default = "r8";
+      r9 = {
+        nix = import ../hackage/req-3.13.4-r9-4f4162faa1591ace42b1bd3039a69a58102e5c5fbcd2fdfdd9dfbf31bb28c9dd.nix;
+        revNum = 9;
+        revTimestamp = "2026-05-01T14:14:19Z";
+        sha256 = "4f4162faa1591ace42b1bd3039a69a58102e5c5fbcd2fdfdd9dfbf31bb28c9dd";
+      };
+      default = "r9";
     };
   };
   "3.2.0" = {
