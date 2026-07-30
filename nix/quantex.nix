3126dca8f3d5ac6c8839234129659021b40d731a
pranaysashank <pranaysashank@users.noreply.github.com>
2026-07-30 00:39:39 +0000

    Automatic Update

---
+++ nix/quantex.nix
@@ -0,0 +1,14 @@
+{
+  "0.1.0.0" = {
+    sha256 = "1d2e9e2c170d95f17cd206d2cb1d35b620130276797a0051986088b012b05b43";
+    revisions = {
+      r0 = {
+        nix = import ../hackage/quantex-0.1.0.0-r0-e537335aaea9043028423cb911e38a333c1be1e422fae766e3ec52dca5abb4a3.nix;
+        revNum = 0;
+        revTimestamp = "2026-07-29T20:50:07Z";
+        sha256 = "e537335aaea9043028423cb911e38a333c1be1e422fae766e3ec52dca5abb4a3";
+      };
+      default = "r0";
+    };
+  };
+}
